d6b12bb38edfbe55a5d271993087cf22460950c9
Peter Marshall <peter.marshall@ocsbstudent.ca>
2021-02-23 15:34:28 +0000

    Add game

---
+++ script.js
@@ -0,0 +1,22 @@
+let myAge;
+let answer;
+
+function age () {
+  myAge = Math.floor(Math.random() * 90) + 5
+  while (myAge !== answer) {
+    answer = parseInt(prompt('What is my guess?'))
+    if (myAge === answer) {
+      if (confirm('You got it right! Do you want to play again?')) {
+        age()
+      }
+    } else if (answer > myAge) {
+      alert('You guessed too big. Please try again.')
+    } else if (answer < myAge) {
+      alert('You guessed too small. Please try again.')
+    } else {
+      alert('Invalid answer. Please try again.')
+    }
+  }
+}
+
+age()
